38500d63fb723904a3bed45397634668d604f50f
ckalantzis <christos.kalantzis@gmail.com>
2015-12-28 15:03:03 -0800

    Dropped the whole read repair thing and switched to reading ALL, which will have a similar impact of repairing the table.

--- tickler.py
+++ tickler.py
@@ -2,6 +2,7 @@ import sys
 import time
 from cassandra.cluster import Cluster
 from cassandra.query import SimpleStatement
+from cassandra import ConsistencyLevel
 
 # Get the arguments
 cass_keyspace = sys.argv[1]  # Cassandra Keyspace containing the Table to tickle
@@ -19,36 +20,27 @@ cass_throttle = sys.argv[5]  # microseconds
 # Set the connections to the cluster
 cluster = Cluster(
     [cass_ip],
-    port=cass_port, protocol_version=2, compression=False)
+    port=cass_port, compression=False)
 
 # Connect to the KS
 session = cluster.connect(cass_keyspace)
-print 'Connected to Cluster'
 
-# Store existing read repair chance and set to 1
-cass_table_schema = cluster.metadata.keyspaces[cass_keyspace].tables[cass_table]
-orig_read_repair_chance = cass_table_schema.options.get('read_repair_chance')
-print 'Original read repair chance value of ' + str(orig_read_repair_chance) + ' saved.'
-
-session.execute('ALTER TABLE ' + cass_table + ' WITH read_repair_chance = 1;')
-print 'Read repair chance set to 1'
-
+# Get the PK column
+# TODO get the PK column
 
 # read every key of the table
-query = "SELECT * FROM " + cass_table  # kv contains 1000000 rows
+query = 'SELECT id FROM ' + cass_table
 statement = SimpleStatement(query, fetch_size=1000)
 print 'Starting to repair table ' + cass_table
 row_count = 0
 for user_row in session.execute(statement):
     # print user_row.id  # debug
     row_count += 1
+    repair_query = 'SELECT COUNT(1) FROM ' + cass_table + ' WHERE id = ' + str(user_row.id)
+    repair_statement = SimpleStatement(repair_query, consistency_level=ConsistencyLevel.ALL)
+    session.execute(repair_statement)
     time.sleep(float(cass_throttle) / 1000000)  # delay in microseconds between reading each row
     if (row_count % 1000) == 0:
         print str(row_count) + ' rows processed'
 print 'Repair of table ' + cass_table + ' complete'
 print str(row_count) + ' rows read and repaired'
-
-
-# Set read repair back to original value
-session.execute('ALTER TABLE ' + cass_table + ' WITH read_repair_chance = ' + str(orig_read_repair_chance) + ';')
-print 'Read repair chance set back to original value ' + str(orig_read_repair_chance)
